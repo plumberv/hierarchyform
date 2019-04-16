1ec844feb827506ed64f3c9f4a142b60167729ee
Kan <Vicko.Kan@td.com>
2019-04-16 10:16:10 -0400

    Signed-off-by: vickokan@hotmail.com initial committ

---
+++ form1/Form1.Designer.cs
@@ -0,0 +1,158 @@
+﻿namespace form1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.treeView1 = new System.Windows.Forms.TreeView();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(1016, 671);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(324, 68);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Regenerate";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // treeView1
+            // 
+            this.treeView1.Location = new System.Drawing.Point(25, 27);
+            this.treeView1.Name = "treeView1";
+            this.treeView1.Size = new System.Drawing.Size(966, 712);
+            this.treeView1.TabIndex = 1;
+            // 
+            // button2
+            // 
+            this.button2.CausesValidation = false;
+            this.button2.Location = new System.Drawing.Point(1016, 763);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(147, 63);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Expand";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(25, 800);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(966, 26);
+            this.textBox1.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(1012, 27);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(74, 20);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Message";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(1192, 763);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(148, 63);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "Collapse";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(21, 763);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(291, 20);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Input File (in Name,Id,Name,Id... format)";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(1016, 71);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(324, 285);
+            this.textBox2.TabIndex = 7;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(1012, 382);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(51, 20);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "label3";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1368, 853);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.treeView1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Generate Hierarchy From File";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TreeView treeView1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ form1/Form1.cs
@@ -0,0 +1,190 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using Microsoft.VisualBasic.FileIO;
+
+namespace form1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private static DataTable GetDataTabletFromCSVFile(string csv_file_path)
+        {
+            DataTable csvData = new DataTable();
+
+            using (TextFieldParser csvReader = new TextFieldParser(csv_file_path))
+            {
+                csvReader.SetDelimiters(new string[] { "," });
+                csvReader.HasFieldsEnclosedInQuotes = true;
+                string[] colFields = csvReader.ReadFields();
+                foreach (string column in colFields)
+                {
+                    DataColumn datecolumn = new DataColumn(column);
+                    datecolumn.AllowDBNull = true;
+                    csvData.Columns.Add(datecolumn);
+                }
+                while (!csvReader.EndOfData)
+                {
+                    string[] fieldData = csvReader.ReadFields();
+                    //Making empty value as null
+                    for (int i = 0; i < fieldData.Length; i++)
+                    {
+                        if (fieldData[i] == "")
+                        {
+                            fieldData[i] = null;
+                        }
+                    }
+                    csvData.Rows.Add(fieldData);
+                }
+            }
+            return csvData;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            try // main event
+            {
+
+                TreeNode l1node, l2node, l3node, l4node, l5node;
+
+                string l1prev="", l2prev="", l3prev="", l4prev = "", l5prev = "";
+                int l1i = -1, l2i = -1, l3i = -1, l4i = -1, l5i = -1;
+                int i = 0;
+
+                treeView1.Nodes.Clear();                                      
+                DataTable dt1 = GetDataTabletFromCSVFile(textBox1.Text);
+
+                if (dt1 != null) // only process if there are rows loaded
+                {
+                    treeView1.ShowNodeToolTips = true;
+                    textBox2.Text = "No of rows loaded: " + dt1.Rows.Count.ToString();
+
+                    for (i = 0; i < dt1.Rows.Count; i++)
+                    {
+                        //node.ToolTipText = "abcvalue";
+                        //subnode.Text = "subText";
+                        //node.Nodes.Add(subnode);
+                        //node.ToolTipText = "tooltip";
+                        //node.Tag = "tag";
+                        //treeView1.ShowNodeToolTips = true;
+
+                        if (l1prev != dt1.Rows[i][0].ToString()) // new level 1
+                        {
+                            l1i++;
+                            l1node = new TreeNode();
+                            l1node.Text = dt1.Rows[i][0].ToString();
+                            l1node.ToolTipText = dt1.Rows[i][1].ToString();
+                            treeView1.Nodes.Add(l1node);
+                            l2i = -1;
+                            l3i = -1;
+                            l4i = -1;
+                            l5i = -1;
+                            l2prev = "";
+                            l3prev = "";
+                            l4prev = "";
+                            l5prev = "";
+                        }
+
+                        if ((dt1.Columns.Count > 2 ) && (l2prev != dt1.Rows[i][2].ToString())) // new level 2
+                        {
+                            l2i++;
+                            l2node = new TreeNode();
+                            l2node.Text = dt1.Rows[i][2].ToString();
+                            l2node.ToolTipText = dt1.Rows[i][3].ToString();
+                            treeView1.Nodes[l1i].Nodes.Add(l2node);
+                            l3i = -1;
+                            l4i = -1;
+                            l5i = -1;
+                            l3prev = "";
+                            l4prev = "";
+                            l5prev = "";
+                        }
+
+                        if ((dt1.Columns.Count > 4 ) && (l3prev != dt1.Rows[i][4].ToString()) && (dt1.Rows[i][4].ToString() != "")) // new level 3
+                        {
+                            l3i++;
+                            l3node = new TreeNode();
+                            l3node.Text = dt1.Rows[i][4].ToString();
+                            l3node.ToolTipText = dt1.Rows[i][5].ToString();
+                            treeView1.Nodes[l1i].Nodes[l2i].Nodes.Add(l3node);
+                            l4i = -1;
+                            l5i = -1;
+                            l4prev = "";
+                            l5prev = "";
+                        }
+
+                        if ((dt1.Columns.Count > 6 ) && (l4prev != dt1.Rows[i][6].ToString()) && (dt1.Rows[i][6].ToString() != "")) // new level 3
+                        {
+                            l4i++;
+                            l4node = new TreeNode();
+                            l4node.Text = dt1.Rows[i][6].ToString();
+                            l4node.ToolTipText = dt1.Rows[i][7].ToString();
+                            treeView1.Nodes[l1i].Nodes[l2i].Nodes[l3i].Nodes.Add(l4node);
+                            l5i = -1;
+                            l5prev = "";
+                        }
+
+                        if ((dt1.Columns.Count > 8) && (l5prev != dt1.Rows[i][8].ToString()) && (dt1.Rows[i][8].ToString() != "")) // new level 3
+                        {
+                            l5i++;
+                            l5node = new TreeNode();
+                            l5node.Text = dt1.Rows[i][8].ToString();
+                            l5node.ToolTipText = dt1.Rows[i][9].ToString();
+                            treeView1.Nodes[l1i].Nodes[l2i].Nodes[l3i].Nodes[l4i].Nodes.Add(l5node);
+                        }
+
+                        l1prev = dt1.Rows[i][0].ToString();
+                        if (dt1.Columns.Count > 2) l2prev = dt1.Rows[i][2].ToString();
+                        if (dt1.Columns.Count > 4) l3prev = dt1.Rows[i][4].ToString();
+                        if (dt1.Columns.Count > 6) l4prev = dt1.Rows[i][6].ToString();
+                        if (dt1.Columns.Count > 8) l5prev = dt1.Rows[i][8].ToString();
+
+                    }
+                }
+
+            }
+            catch (Exception ex)
+            {
+                textBox2.Text = ex.Message;
+            }
+             
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            treeView1.ExpandAll();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            treeView1.CollapseAll();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // put under initialization event
+            textBox1.Text = @"C:\Vicko\WDP\projects\mlorg\form1\branchhierarchy.csv";
+            label3.Text = "Note: \n" +
+                "1. Source file in Name,Id,Name,Id...format\n" +
+                "2. Order by Name, and max 5 levels(pairs)\n" +
+                "3. csv generated via saved query in Cinchy\n" +
+                "4. See Get Wealth Branch Hierarchy HR/WBR..";
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+            // none
+        }
+    }
+}
